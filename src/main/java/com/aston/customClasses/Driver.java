08183f8449dd962efb3834ba997af60436229151
Faina Tyupaeva <tuypaevafaina@gmail.com>
2025-10-04 01:20:55 +0300

    Add Validation For Driver, Route Add BinarySearch

--- src/main/java/com/aston/customClasses/Driver.java
+++ src/main/java/com/aston/customClasses/Driver.java
@@ -1,6 +1,5 @@
+
 package com.aston.customClasses;
-
-
 import java.util.Comparator;
 import java.util.Objects;
 
@@ -10,24 +9,6 @@ public class Driver {
     private final int experience;//стаж водителя
     private final int age; // возраст водителя          ---аккуратнее с рандомом, а то лет 40, а стаж 25 нехорошо---
     private final double rate; // рейтинг водителя
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Driver driver = (Driver) obj;
-        return experience == driver.experience &&
-                age == driver.age &&
-                Double.compare(driver.rate, rate) == 0 &&
-                Objects.equals(name, driver.name) &&
-                Objects.equals(category, driver.category);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, category, experience, age, rate);
-    }
-
 
     public static Comparator<Driver> compareByName() {
         return Comparator.comparing(Driver::getName);
@@ -50,12 +31,19 @@ public class Driver {
             }
         };
     }
-
-
-
-
-
-
+    public static Comparator<Driver> compare() {
+        return new Comparator<Driver>() {
+            @Override
+            public int compare(Driver driver1, Driver driver2) {
+                if(driver1.equals(driver2))
+                    return 0;
+                else if (driver1.hashCode() > driver2.hashCode())
+                    return 1;
+                else
+                    return -1;
+            }
+        };
+    }
 
     private Driver(DriverBuilder driverBuilder){
         this.name = driverBuilder.name;
@@ -89,6 +77,28 @@ public class Driver {
         return String.format("\n%s\nВозраст:%s\nКатегория:%s\nСтаж:%s лет\nРейтинг:%s⭐\n",getName(),getAge(),getCategory(),getExperience(),getRate());
     }
 
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(category);
+        result = 31 * result + experience;
+        result = 31 * result + age;
+        result = 31 * result + Double.hashCode(rate);
+        return result;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Driver driver = (Driver) o;
+
+        return experience == driver.experience &&
+                age == driver.age &&
+                Double.compare(driver.rate, rate) == 0 &&
+                Objects.equals(name, driver.name) &&
+                Objects.equals(category, driver.category);
+    }
 
     public int getAge() {
         return age;
@@ -118,9 +128,6 @@ public class Driver {
             this.experience = experience;
             return this;
         }
-        public Driver build(){
-            return new Driver(this);
-        }
 
         public DriverBuilder setAge(int age) {
             this.age = age;
@@ -131,6 +138,16 @@ public class Driver {
             this.rate = rate;
             return this;
         }
+        public Driver build() {
+            if (this.name == null || this.category == null || this.experience == 0)
+                throw new RuntimeException("Зполните все обязательные параметры: Имя, категория и водительский стаж!");
+            else if(this.age < 21 || this.age > 100)
+                throw new RuntimeException("Возраст должен быть в диапазоне 21-100 лет включительно!");
+            else if(this.age - this.experience < 18)
+                throw new RuntimeException("Некорректный водительский стаж, водить можно с 18 лет!");
+            else if(this.rate != 0 && (this.rate > 5 || this.rate < 0))
+                throw new RuntimeException("Рейтинг водителя должен быть в диапазоне от 0.0 до 5.0 включительно!");
+            else return new Driver(this);
+        }
     }
-
 }
